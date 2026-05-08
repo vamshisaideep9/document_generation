c3964a5ebf0fd22a17c3ca7613e53a326b185c49
vamshisaideep9 <vamshisaideep@gmail.com>
2026-05-08 15:34:44 +0530

    updated code

--- cert_engine/app/main.py
+++ cert_engine/app/main.py
@@ -27,7 +27,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=["https://www.brollysolutions.in"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
